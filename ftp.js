b69411f458dccff781c092039e6839ddbbbea1e1
Brian White <mscdex@mscdex.net>
2012-08-02 03:09:26 -0400

    Create Date instances instead of raw date/time values

--- ftp.js
+++ ftp.js
@@ -1,11 +1,27 @@
 var util = require('util'),
     net = require('net'),
     EventEmitter = require('events').EventEmitter,
-    XRegExp = require('./xregexp'),
-    reXListUnix = XRegExp.cache('^(?<type>[\\-ld])(?<permission>([\\-r][\\-w][\\-xs]){3})\\s+(?<inodes>\\d+)\\s+(?<owner>\\w+)\\s+(?<group>\\w+)\\s+(?<size>\\d+)\\s+(?<timestamp>((?<month1>\\w{3})\\s+(?<date1>\\d{1,2})\\s+(?<hour>\\d{1,2}):(?<minute>\\d{2}))|((?<month2>\\w{3})\\s+(?<date2>\\d{1,2})\\s+(?<year>\\d{4})))\\s+(?<name>.+)$'),
+    XRegExp = require('./xregexp');
+
+var reXListUnix = XRegExp.cache('^(?<type>[\\-ld])(?<permission>([\\-r][\\-w][\\-xs]){3})\\s+(?<inodes>\\d+)\\s+(?<owner>\\w+)\\s+(?<group>\\w+)\\s+(?<size>\\d+)\\s+(?<timestamp>((?<month1>\\w{3})\\s+(?<date1>\\d{1,2})\\s+(?<hour>\\d{1,2}):(?<minute>\\d{2}))|((?<month2>\\w{3})\\s+(?<date2>\\d{1,2})\\s+(?<year>\\d{4})))\\s+(?<name>.+)$'),
     reXListMSDOS = XRegExp.cache('^(?<month>\\d{2})(?:\\-|\\/)(?<date>\\d{2})(?:\\-|\\/)(?<year>\\d{2,4})\\s+(?<hour>\\d{2}):(?<minute>\\d{2})\\s{0,1}(?<ampm>[AaMmPp]{1,2})\\s+(?:(?<size>\\d+)|(?<isdir>\\<DIR\\>))\\s+(?<name>.+)$'),
     reXTimeval = XRegExp.cache('^(?<year>\\d{4})(?<month>\\d{2})(?<date>\\d{2})(?<hour>\\d{2})(?<minute>\\d{2})(?<second>\\d+)$'),
     reKV = /(.+?)=(.+?);/;
+
+var MONTHS = {
+  jan: 1,
+  feb: 2,
+  mar: 3,
+  apr: 4,
+  may: 5,
+  jun: 6,
+  jul: 7,
+  aug: 8,
+  sep: 9,
+  oct: 10,
+  nov: 11,
+  dec: 12
+};
 
 var FTP = module.exports = function(options) {
   this._socket = undefined;
@@ -34,7 +50,7 @@ FTP.prototype.connect = function(port, host) {
   this.options.port = port = port || this.options.port;
   this.options.host = host = host || this.options.host;
 
-  this._feat = new Object();
+  this._feat = {};
 
   if (socket)
     socket.end();
@@ -439,16 +455,15 @@ FTP.prototype.lastMod = function(path, cb) {
   return this.send('MDTM', path, function(e, text) {
     if (e)
       return cb(e);
-    var val = reXTimeval.exec(text), ret;
+    var val = reXTimeval.exec(text),
+        ret;
     if (!val)
       return cb(new Error('Invalid date/time format from server'));
-    ret = new Object();
-    ret.year = parseInt(val.year, 10);
-    ret.month = parseInt(val.month, 10);
-    ret.date = parseInt(val.date, 10);
-    ret.hour = parseInt(val.hour, 10);
-    ret.minute = parseInt(val.minute, 10);
-    ret.second = parseFloat(val.second, 10);
+    // seconds can be a float, we'll just truncate this because Date doesn't
+    // support fractions of a second
+    var secs = parseInt(val.second, 10);
+    ret = new Date(val.year + '-' + val.month + '-' + val.date + 'T' + val.hour
+                   + ':' + val.minute + ':' + secs);
     cb(undefined, ret);
   });
 };
@@ -601,7 +616,7 @@ function processDirLines(lines, emitter, type, debug) {
 }
 
 function parseResponses(lines) {
-  var resps = new Array(),
+  var resps = [],
       multiline = '';
   for (var i=0,match,len=lines.length; i<len; ++i) {
     if (match = lines[i].match(/^(\d{3})(?:$|(\s|\-)(.+))/)) {
@@ -625,7 +640,7 @@ function parseResponses(lines) {
 function parseMList(line) {
   var ret, result = line.trim().split(reKV);
   if (result && result.length > 0) {
-    ret = new Object();
+    ret = {};
     if (result.length === 1)
       ret.name = result[0].trim();
     else {
@@ -643,44 +658,52 @@ function parseMList(line) {
 }
 
 function parseList(line) {
-  var ret, info, thisYear = (new Date()).getFullYear(),
-      months = {
-        jan: 1,
-        feb: 2,
-        mar: 3,
-        apr: 4,
-        may: 5,
-        jun: 6,
-        jul: 7,
-        aug: 8,
-        sep: 9,
-        oct: 10,
-        nov: 11,
-        dec: 12
-      };
+  var ret,
+      info,
+      thisYear = (new Date()).getFullYear(),
+      month,
+      day,
+      year,
+      hour,
+      mins;
 
   if (ret = reXListUnix.exec(line)) {
-    info = new Object();
-    info.type = ret.type;
-    info.rights = new Object();
-    info.rights.user = ret.permission.substring(0, 3).replace('-', '');
-    info.rights.group = ret.permission.substring(3, 6).replace('-', '');
-    info.rights.other = ret.permission.substring(6, 9).replace('-', '');
-    info.owner = ret.owner;
-    info.group = ret.group;
-    info.size = ret.size;
-    info.date = {};
-    if (typeof ret.month1 !== 'undefined') {
-      info.date.month = parseInt(months[ret.month1.toLowerCase()], 10);
-      info.date.date = parseInt(ret.date1, 10);
-      info.date.year = thisYear;
-      info.time = new Object();
-      info.time.hour = parseInt(ret.hour, 10);
-      info.time.minute = parseInt(ret.minute, 10);
-    } else if (typeof ret.month2 !== 'undefined') {
-      info.date.month = parseInt(months[ret.month2.toLowerCase()], 10);
-      info.date.date = parseInt(ret.date2, 10);
-      info.date.year = parseInt(ret.year, 10);
+    info = {
+      type: ret.type,
+      rights: {
+        user: ret.permission.substring(0, 3).replace('-', ''),
+        group: ret.permission.substring(3, 6).replace('-', ''),
+        other: ret.permission.substring(6, 9).replace('-', '')
+      },
+      owner: ret.owner,
+      group: ret.group,
+      size: ret.size,
+      date: undefined
+    };
+    if (ret.month1 !== undefined) {
+      month = parseInt(MONTHS[ret.month1.toLowerCase()], 10);
+      day = parseInt(ret.date1, 10);
+      year = thisYear;
+      hour = parseInt(ret.hour, 10);
+      mins = parseInt(ret.minute, 10);
+      if (month < 10)
+        month = '0' + month;
+      if (day < 10)
+        day = '0' + day;
+      if (hour < 10)
+        hour = '0' + hour;
+      if (mins < 10)
+        mins = '0' + mins;
+      info.date = new Date(year + '-' + month + '-' + day + 'T' + hour + ':' + mins);
+    } else if (ret.month2 !== undefined) {
+      month = parseInt(MONTHS[ret.month2.toLowerCase()], 10);
+      day = parseInt(ret.date2, 10);
+      year = parseInt(ret.year, 10);
+      if (month < 10)
+        month = '0' + month;
+      if (day < 10)
+        day = '0' + day;
+      info.date = new Date(year + '-' + month + '-' + day);
     }
     if (ret.type === 'l') {
       var pos = ret.name.indexOf(' -> ');
@@ -690,21 +713,33 @@ function parseList(line) {
       info.name = ret.name;
     ret = info;
   } else if (ret = reXListMSDOS.exec(line)) {
-    info = new Object();
-    info.type = (ret.isdir ? 'd' : '-');
-    info.size = (ret.isdir ? '0' : ret.size);
-    info.date = new Object();
-    info.date.month = parseInt(ret.month, 10);
-    info.date.date = parseInt(ret.date, 10);
-    info.date.year = parseInt(ret.year, 10);
-    info.time = new Object();
-    info.time.hour = parseInt(ret.hour, 10);
-    info.time.minute = parseInt(ret.minute, 10);
-    if (ret.ampm[0].toLowerCase() === 'p' && info.time.hour < 12)
-      info.time.hour += 12;
-    else if (ret.ampm[0].toLowerCase() === 'a' && info.time.hour === 12)
-      info.time.hour = 0;
-    info.name = ret.name;
+    info = {
+      name: ret.name,
+      type: (ret.isdir ? 'd' : '-'),
+      size: (ret.isdir ? '0' : ret.size),
+      date: undefined,
+    };
+    month = parseInt(ret.month, 10),
+    day = parseInt(ret.date, 10),
+    year = parseInt(ret.year, 10),
+    hour = parseInt(ret.hour, 10),
+    mins = parseInt(ret.minute, 10);
+
+    if (ret.ampm[0].toLowerCase() === 'p' && hour < 12)
+      hour += 12;
+    else if (ret.ampm[0].toLowerCase() === 'a' && hour === 12)
+      hour = 0;
+
+    if (month < 10)
+      month = '0' + month;
+    if (day < 10)
+      day = '0' + day;
+    if (hour < 10)
+      hour = '0' + hour;
+    if (mins < 10)
+      mins = '0' + mins;
+
+    info.date = new Date(year + '-' + month + '-' + day + 'T' + hour + ':' + mins);
     ret = info;
   } else
     ret = line; // could not parse, so at least give the end user a chance to
