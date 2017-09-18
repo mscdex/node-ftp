7f756138bca540a48834383147a01d5e27c6652f
Tamás András Horváth <htomy92@gmail.com>
2017-09-18 15:59:25 +0200

    Add for test

--- test/test-connection.js
+++ test/test-connection.js
@@ -5,16 +5,24 @@ c.on('ready', function() {
   c.list(function(err, list) {
     if (err) throw err;
     console.dir(list);
-    c.end();
+    //c.end();
+  });
+
+  c.on('error', function(err) {
+    console.error('ERROR: ', err);
+  });
+
+  c.on('close', function(err) {
+    console.error('CLOSE: ', err);
   });
 });
 
 c.connect({
-    host: 'ipv6.app',
-    //host: '192.168.10.10',
-    user: 'icetee',
+    //host: 'ipv6.app',
+    host: 'localhost',
+    //user: 'icetee',
+    //password: 'password',
     //forcePasv: true,
-    password: 'password', // ;)
     debug: function(text) {
         console.log(text);
     }
