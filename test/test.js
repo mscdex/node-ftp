8e154e9915fdca2fd1dcdb4c6e528919771c91c9
Tamás András Horváth <htomy92@gmail.com>
2017-10-25 21:44:17 +0200

    Developer enviroments and add Mocha

--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
-require('fs').readdirSync(__dirname).forEach(function(f) {
-  if (f.substr(0, 5) === 'test-')
-    require('./' + f);
+const assert = require('assert');
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
 });
